70e4923e36df950145e8203508a2d290c935c956
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-08 23:56:33 +0530

    2 Commit

---
+++ PrimeNum.java
@@ -0,0 +1,18 @@
+public class PrimeNum {
+    public static void main(String[] args) {
+        int n = 17;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            System.out.println("Yes, Prime");
+        } else {
+            System.out.println("No, Not Prime");
+        }
+    }
+}
