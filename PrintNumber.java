ca4c7c02b5d200b076ffe26397af0cc4716e141e
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-11 23:10:43 +0530

    3 Commit

---
+++ PrintNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Number is " + n );
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}
