0b45e284e5c561d05873bef01dc64c385772a477
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-12 18:48:27 +0530

    6 Commit

---
+++ PoweExponentiation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PoweExponentiation {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number (x): ");
+        int x = sc.nextInt();
+
+        System.out.print("Enter the power (n): ");
+        int n = sc.nextInt();
+
+        int ans = 1;
+
+        while (n > 0) {
+            if (n % 2 == 1) { 
+                ans = ans * x;
+                n = n - 1;
+            } else {
+                x = x * x;
+                n = n / 2;
+            }
+        }
+
+        System.out.println("Answer = " + ans);
+    }
+}
