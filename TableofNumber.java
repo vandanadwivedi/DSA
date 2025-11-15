1bc4f63026f440fd22a3cf5d71204224c3c5cb34
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-15 23:04:04 +0530

    12 Commit

---
+++ TableofNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TableofNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+System.out.println("Table " );
+        for(int i=1;i<10;i++)
+        {
+            System.out.println(num + "x"+ i + "=" +(num*i));
+        }
+        
+    }
+}
