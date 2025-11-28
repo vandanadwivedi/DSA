2cdc8e41950e9c5ecc0c6779d58956d91391a6d0
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-28 22:24:02 +0530

    22 Commit

--- SumofDigit.java
+++ SumofDigit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfDigit {
+public class SumofDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
