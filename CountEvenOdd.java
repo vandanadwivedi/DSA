6ac8da295136916a44f30817ef63c1c7423c619d
vandanadwivedi8630@gmail.com <vandanadwivedi8630@gmail.com>
2025-11-28 21:36:35 +0530

    21 Commit

---
+++ CountEvenOdd.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int even = 0, odd = 0;
+
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even numbers: " + even);
+        System.out.println("Odd numbers: " + odd);
+    }
+}
